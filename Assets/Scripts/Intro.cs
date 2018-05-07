1ca8d339679af90a26f7c73a5c43f16d9e0acdda
wijayaerick <wijaya.erick52@gmail.com>
2018-05-07 11:35:26 +0700

    skip button

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -14,7 +14,7 @@ public class Intro : MonoBehaviour {
 	void Start () {
 		rt = GetComponent<RectTransform>();
 		texts = GetComponentsInChildren<Text>();
-		for (int i = 0; i < texts.Length; i++) {
+		for (int i = 0; i < texts.Length - 1; i++) {
 			texts[i].gameObject.SetActive(false);
 		}
 		texts[0].gameObject.SetActive(true);
@@ -26,13 +26,20 @@ public class Intro : MonoBehaviour {
 		if (curTime > 2f) {
 			curTime = 0;
 			index++;
-			if (index >= texts.Length) {
+			if (index >= texts.Length - 1) {
 				Debug.Log("Done");
 				SceneManager.LoadScene(3);
+			} 
+			else {
+				texts[index-1].gameObject.SetActive(false);
+				texts[index].gameObject.SetActive(true);
 			}
-			texts[index-1].gameObject.SetActive(false);
-			texts[index].gameObject.SetActive(true);
 		}
-		
+	}
+
+	public void Skip() {
+		texts[index-1].gameObject.SetActive(false);
+		texts[index].gameObject.SetActive(false);
+		index = texts.Length - 1;
 	}
 }
